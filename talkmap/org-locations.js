836fbf0c91243b9238d814e6da6984430c04d0b5
aydoganyanilmaz <34016413+aydoganyanilmaz@users.noreply.github.com>
2018-01-06 10:43:24 -0500

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,27 +1,70 @@
 var addressPoints = [
   [
-    "Berlin, Germany", 
+    "The costs and benefits of processing negation in NPI licensing contexts in Turkish|XPRAG, ZAS", 
     52.5170365, 
     13.3888599
   ], 
   [
-    "Los Angeles, California", 
+    "Anti-local intrusion effects in Turkish|TU+3, UCLA", 
     34.1430079, 
     -118.14176172581
   ], 
   [
-    "Abu Dhabi, UAE", 
-    48.4593031, 
-    35.0670472
+    "Intrusion effects on NPI licensing in Turkish: Does the parser ignore the grammar?|EXAL+ NYU Abu Dhabi", 
+    24.5253792, 
+    54.4312089
   ], 
   [
-    "Harvard University", 
+    "Online processing of grammatical illusions:evidence from Turkish NPI licensing (Invited)|Language & Cognition Seminar, Harvard University", 
     42.36782045, 
     -71.1266665287448
   ], 
   [
-    "Amherst, MA", 
+    "Illusions of grammaticality on NPI licensing in Turkish: Does the parser ignore the grammar?|TU+1, UMass", 
     42.3803676, 
     -72.523143
+  ]
+  , 
+  [
+    "Illusions of grammaticality, prospective NPI/licensor dependencies (Invited)|Psycholinguistics Supper, CUNY", 
+    40.7486525, 
+    -73.986201
+  ]
+  , 
+  [
+    "Illusions of grammaticality on NPI licensing in Turkish: Does the parser ignore the grammar?|CUNY Sentence Processing, OSU", 
+    40.0137092, 
+    -83.031445
+  ]
+  , 
+  [
+    "Processing of Negative Polarity Items in Turkish|Neurobiology of Language Conference, San Diego", 
+    32.7160409, 
+    -117.1691961
+  ], 
+  [
+    "NPI Licensing in Turkish: An ERP Study|Cognitive Neuroscience Society Meeting, San Francisco", 
+    37.7943762, 
+    -122.398037
+  ], 
+  [
+    "A-not-A Questions in Turkish|WAFL 13, University of Stuttgart", 
+    48.7816507, 
+    9.1703045
+  ], 
+  [
+    "On the Syntax of Embedded Yes/No Questions in Turkish|SYNC 13, NYU", 
+    40.7294051, 
+    -73.9971363
+  ], 
+  [
+    "Some Remarks on NPIs in Turkish|Syntax Workshop, METU", 
+    39.9012109, 
+    32.770549
+  ], 
+  [
+    "NPIs in Wh-Questions|22. National Linguistics Conference, Van Yüzüncü Yıl University", 
+    38.570772, 
+    43.2828758
   ]
 ];
